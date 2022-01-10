0e39b199524f8f8dcc76ffc7f269221df4ba1389
wulandarikaranamu <wulandarikaranamu02@gmail.com>
2022-01-10 19:26:32 +0700

    merry

---
+++ for.py
@@ -0,0 +1,22 @@
+#ngoding yuk
+
+#menampilkan perulangan angka ganjil
+angka = 100
+for i in range(1,100,2):
+    print(i)
+
+print("="*30)   
+
+#mnampilkan perulangan angka genap
+angka = 100
+for i in range(0,100,2):
+    print(i)
+
+
+angka = int(input("masukkan angka :"))
+if angka >= 10 :
+    print("angka genap")
+else:
+    print("angka ganjil")
+    
+                  
